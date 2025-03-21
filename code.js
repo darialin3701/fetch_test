3a27dcfb1438ceb79a38c71556b0adadca4ceaeb
darialin3701 <darakulikovskaia09@gmail.com>
2025-03-21 20:03:52 +0300

    Update code.js

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const apiKey = 'ebbe750a60644d6bb7f160946252103';
 
   const fetchData = async (cityName) => {
-    const response = await feath('https://api.weatherapi.com/v1/current.json?key=${apiKey}=${cityName}&aqi=no&lang=ru')
+    const response = await feath('https://api.weatherapi.com/v1/current.json?key=${apiKey}03=${cityName}&aqi=no&lang=ru')
     console.log(response);
   }
   
