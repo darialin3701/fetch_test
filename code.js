7f68855afc58891b336e1edec20c58fcee942fb2
darialin3701 <darakulikovskaia09@gmail.com>
2025-03-21 22:02:06 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,10 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const apiKey = 'ebbe750a60644d6bb7f160946252103';
-
   const searchText = document.getElementById('searchText');
   const buttonSearch = document.getElementById('buttonSearch');
   const tableData = document.getElementById('tableData').querySelector('tbody');
-
   const fetchData = async (cityName) => {
     try {
       const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no&lang=ru`)
@@ -14,7 +12,6 @@ document.addEventListener("DOMContentLoaded", () => {
       console.log('error', error);
     }
   }
-  
   buttonSearch.addEventListener('click', async () => {
     const name = searchText.value;
     if (!name) {
