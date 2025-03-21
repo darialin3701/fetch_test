1d41bf8552d458a5e500fa78c40b64c8f624c66c
darialin3701 <darakulikovskaia09@gmail.com>
2025-03-21 21:30:55 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const apiKey = 'ebbe750a60644d6bb7f160946252103';
 
-  const searchText = document.getElementById('searchText");
-  const buttonSearch = document.getElementById('buttonSearch");
-  const tableData = document.getElementById('tableData");
+  const searchText = document.getElementById('searchText');
+  const buttonSearch = document.getElementById('buttonSearch');
+  const tableData = document.getElementById('tableData');
 
   const fetchData = async (cityName) => {
     try {
