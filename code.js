6ce234eca2e0438fa77181505002b9fc6a849f98
darialin3701 <darakulikovskaia09@gmail.com>
2025-03-21 19:33:31 +0300

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const apiKey = 'ebbe750a60644d6bb7f160946252103';
+
+  try {
+    const response = await feath ('http://api.weatherapi.com/v1/current.json?key=ebbe750a60644d6bb7f160946252103&q=London&aqi=no')
+    console.log(response);
+  } catch (error) {
+    console.log(error)
+  }
 });
